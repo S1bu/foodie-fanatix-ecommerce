75767f5f6baa3b2d057f5e5bd3b4e25329b7dabe
S1bu <74048211+S1bu@users.noreply.github.com>
2023-11-17 14:18:22 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -15,7 +15,508 @@ const meals = [
 },
 {
     id:3,
+    product:"Hot dog",const meals = [
+{
+    id:1,
+    product:"Juicy Chicken Burger",
+    Image:"https://i.postimg.cc/CMng0LWn/17-Food-Word-Origin-Stories-That-Will-Make-You-Hungry.jpg",
+    price:"21",
+    category:"meal"
+},
+{
+    id:2,
+    product:"Crispy Chicken Tacos",
+    Image:"https://i.postimg.cc/gjjJ4qnS/Crispy-Chicken-Carnitas-Tacos-with-Avocado-Cilantro-Lime-Sauce.jpg",
+    price:"21",
+    category:"meal"
+},
+{
+    id:3,
     product:"Hot dog",
+    Image:"https://i.postimg.cc/RV83s81c/hot-dog.jpg",
+    price:"21",
+    category:"meal"
+},
+{
+    id:4,
+    product:"Sphagetti and meat sauce",
+    Image:"https://i.postimg.cc/6QVyQpk0/The-Best-Spaghetti-and-Meat-Sauce-with-Red-Wine-Joyous-Apron.jpg",
+    price:"21",
+    category:"meal"
+},
+{
+    id:5,
+    product:"Pizza",
+    Image:"https://i.postimg.cc/fTktvZdz/pizza.jpg",
+    price:"21",
+    category:"meal"
+},
+{
+    id:6,
+    product:"Fish and Chips",
+    Image:"https://i.postimg.cc/Gpn4ry5t/Fish-and-chips.jpg",
+    price:"21",
+    category:"meal"
+}
+
+]
+
+const deserts = [
+    {
+        id:1,
+        product:"Pumpkin Cookie Dough Ice-Cream",
+        Image:"https://i.postimg.cc/cCqK1khp/Butterscotch-Sw-irl-Pumpkin-Cookie-Dough-Ice-Cream-Full-of-Plants.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:2,
+        product:"Chocolate Oreo Pudding Delight",
+        Image:"https://i.postimg.cc/ZKS91gC3/Chocolate-Oreo-Pudding-Delight.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:3,
+        product:"Crispy Chocolate Chip waffles",
+        Image:"https://i.postimg.cc/3wc4VxvJ/Crispy-Chocolate-Chip-Waffles-Baking-Mischief.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:4,
+        product:"Old Fashioned Devils Food Cake",
+        Image:"https://i.postimg.cc/jqNw3Kdt/Old-Fashioned-Devil-s-Food-Cake-Baker-by-Nature.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:5,
+        product:"Peppermint Crisp Tart",
+        Image:"https://i.postimg.cc/B6PL0fT5/Peppermint-Crisp-Fudge-Hein-van-Tonder-Top-Food-Photographer-Stylist-l-Abu-Dhabi.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:6,
+        product:"Milk Tart",
+        Image:"https://i.postimg.cc/fyxVV7gJ/Traditional-South-African-Milk-Tart.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:7,
+        product:"Galaxy Cup cake",
+        Image:" https://i.postimg.cc/d3Qnq9Bj/Galaxy-Cupcakes-Recipe-Frugal-Mom-Eh.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:8,
+        product:"Brownie Sundae",
+        Image:"https://i.postimg.cc/W3vXyVqK/Brownie-Sundae.jpg",
+        price:"21",
+        category:"desert"
+    },
+    {
+        id:9,
+        product:"Magnum Ice cream",
+        Image:"https://i.postimg.cc/B6BM9zxv/Almond-Magnum-Ice-Cream-Vegan-Nutriholist.jpg",
+        price:"21",
+        category:"desert"
+    },
+
+   
+]
+
+const drinks = [
+    {
+        id:1,
+        product:"Water",
+        Image:"https://i.postimg.cc/155x5k5w/water.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:2,
+        product:"Riverdale milkshake",
+        Image:"https://i.postimg.cc/C1GyzYqH/Riverdale-Milkshake.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:3,
+        product:"Score energy drink",
+        Image:"https://i.postimg.cc/KjfdDNQ1/score.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:4,
+        product:"Coke",
+        Image:"https://i.postimg.cc/Z52XH0Px/coke.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:5,
+        product:"Corona",
+        Image:"https://i.postimg.cc/FzNMSqbt/Corona-Extra.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:6,
+        product:"Fanta Orange",
+        Image:"https://i.postimg.cc/jSLGyyfj/Flyer-Fanta-Composi-o-feita-com-Photoshop.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:7,
+        product:"HEINEKEN",
+        Image:"https://i.postimg.cc/441kGfZX/HEINEKEN-BEM-GELADA.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:8,
+        product:"Rum Berry cocktail",
+        Image:"https://i.postimg.cc/Qx62BfHP/New-Year-s-Eve-Rum-Berry-Cocktail.jpg",
+        price:"21",
+        category:"drinks"
+    },
+    {
+        id:9,
+        product:"Perfect Vanilla Bean Iced Coffee",
+        Image:" https://i.postimg.cc/BnwZF40P/5b06de2d0c0da5b8d48f990821cf24aa.jpg",
+        price:"21",
+        category:"drinks"
+    },
+   
+]
+
+const starters =[
+    {
+        id:1,
+        product:"Mini pizza",
+        Image:"https://i.postimg.cc/rpXPB3DB/Mini-pizzatjes.jpg",
+        price:"21",
+        category:"starters"
+    },
+    {
+        id:2,
+        product:"Sushi",
+        Image:"https://i.postimg.cc/7YRcVfwY/Food-and-Drinks.jpg",
+        price:"21",
+        category:"starters"
+    },
+    {
+        id:3,
+        product:"Samoosas",
+        Image:"https://i.postimg.cc/mgFn6Bg3/Chinese-Samosa-Recipe-With-Chicken-and-Noodles-Cubes-N-Juliennes.jpg",
+        price:"21",
+        category:"starters"
+    },
+    {
+        id:4,
+        product:"Wings",
+        Image:"https://i.postimg.cc/FKyBNpc7/Brown-Sugar-BBQ-Baked-Wings.jpg",
+        price:"21",
+        category:"starters"
+    },
+    {
+        id:5,
+        product:"Beef swekers",
+        Image:"https://i.postimg.cc/qRQDC71r/Beef-Shish-Kebab.jpg",
+        price:"21",
+        category:"starters"
+    },
+    {
+        id:6,
+        product:"Chipotle Chicken Nachos with Jalapeño Yogurt",
+        Image:"https://i.postimg.cc/9QzRKxXT/c5a2e8b95258635d16987246ef46f755.jpg",
+        price:"21",
+        category:"starters"
+    },
+    
+]
+
+const special = [
+    {
+        id:1,
+        product:"Beef Burgers x 6",
+        Image:"https://i.postimg.cc/7hWq5cMH/6-Tips-for-Making-the-Best-Burgers-Ever.jpg",
+        price:"15",
+        category:"Special"
+    },
+    {
+        id:2,
+        product:"Custard & Jelly x2",
+        Image:"https://i.postimg.cc/JhN7ppmx/Custard-Jelly-Pudding.jpg",
+        price:"15",
+        category:"Special"
+    },
+    {
+        id:3,
+        product:"Moroccan Chicken Wrap x",
+        Image:"https://i.postimg.cc/qqRkM0KD/Moroccan-Chicken-Wrap.jpg",
+        price:"15",
+        category:"Special"
+    },
+    {
+        id:4,
+        product:"Rasberry mores Milkshake",
+        Image:"https://i.postimg.cc/xCgf2qXb/Raspberry-S-mores-Milkshake-Make-and-Takes.jpg",
+        price:"15",
+        category:"Special"
+    },
+    
+]
+
+// DOM for display
+let specials_display = document.querySelector('.specials');
+
+
+
+//DOM CLICKED
+let meal_display = document.querySelector('#meals')
+let drink_display = document.querySelector('#drinks')
+let desert_display = document.querySelector('#deserts')
+let starter_display = document.querySelector('#starters')
+
+//---- DISPLAYs------
+// specials
+special.forEach(special => {
+    specials_display.innerHTML+=`
+    
+    <div class="card" style="border:1px solid white;">
+               
+    <div class="image-holder">
+        <img src="${special.Image}" alt="">
+    </div>
+    <div class="details">
+        <div class="row">
+            <div class="col">
+               <strong><p>${special.product}</p></strong>
+            </div>
+            <div class="col price">
+                    <p>$${special.price}</p>
+            </div>
+        </div>
+    
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+             impedit placeat ipsum, architecto ipsam iusto praesentium eaque
+        </p>
+        <div class="cart-button">
+        <i class="bi bi-bag"></i>
+        </div>
+    </div>
+</div>
+    `});
+//  meals
+meals.forEach(meal => {
+        meal_display.innerHTML+=`
+        <div class="card" style="border:1px solid white;">       
+        <div class="image-holder">
+            <img src="${meal.Image}" alt="">
+        </div>
+        <div class="details">
+            <div class="row">
+                <div class="col">
+                   <strong><p>${meal.product}</p></strong>
+                </div>
+                <div class="col price">
+                        <p>$${meal.price}</p>
+                </div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                 impedit placeat ipsum, architecto ipsam iusto praesentium eaque
+            </p>
+            <div class="cart-button">
+            <button class="cart-button" onclick="add_to_cart(meals)">
+            <i class="bi bi-bag"></i>
+            </button>
+            </div>
+        </div>
+    </div>
+        `});
+ 
+// deserts
+deserts.forEach(desert => {
+            desert_display.innerHTML+=`
+            <div class="card" style="border:1px solid white;">       
+            <div class="image-holder">
+                <img src="${desert.Image}" alt="">
+            </div>
+            <div class="details">
+                <div class="row">
+                    <div class="col">
+                       <strong><p>${desert.product}</p></strong>
+                    </div>
+                    <div class="col price">
+                            <p>$${desert.price}</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                     impedit placeat ipsum, architecto ipsam iusto praesentium eaque
+                </p>
+                <div class="cart-button">
+                <button class="cart-button" onclick="add_to_cart(deserts)">
+                <i class="bi bi-bag"></i>
+                </button>
+                </div>
+            </div>
+        </div>
+            `});   
+ 
+//drinks
+drinks.forEach(drink => {
+            drink_display.innerHTML+=`
+            <div class="card" style="border:1px solid white;">       
+            <div class="image-holder">
+                <img src="${drink.Image}" alt="">
+            </div>
+            <div class="details">
+                <div class="row">
+                    <div class="col">
+                       <strong><p>${drink.product}</p></strong>
+                    </div>
+                    <div class="col price">
+                            <p>$${drink.price}</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                     impedit placeat ipsum, architecto ipsam iusto praesentium eaque
+                </p>
+                <div class="cart-button">
+                <button class="cart-button" onclick="add_to_cart(drinks)">
+                <i class="bi bi-bag"></i>
+                </button>
+                </div>
+            </div>
+        </div>
+            `}); 
+ 
+//starters
+starters.forEach(starter => {
+            starter_display.innerHTML+=`
+
+            <div class="card" style="border:1px solid white;">       
+            <div class="image-holder">
+                <img src="${starter.Image}" alt="">
+            </div>
+            <div class="details">
+                <div class="row">
+                    <div class="col">
+                       <strong><p>${starter.product}</p></strong>
+                    </div>
+                    <div class="col price">
+                            <p>$${starter.price}</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                     impedit placeat ipsum, architecto ipsam iusto praesentium eaque
+                </p>
+                <div class="cart-button">
+                <button class="cart-button" onclick="add_to_cart(starters)">
+                <i class="bi bi-bag"></i>
+                </button>
+                </div>
+            </div>
+        </div>
+            `});   
+ 
+
+// ----------------------------------
+//-----------CART-------------------
+// ----------------------------------
+let cart = []
+
+let cart_target = document.querySelector('.count')
+
+// Event listeners
+let cart_button = document.querySelectorAll('.cart-button')
+
+// cart-button.addEventListener("click",add_to_cart)
+
+function add_to_cart(starters) {
+    cart.push({
+        id: starters.id,
+        product: starters.product,
+        Image: starters.Image,
+        price: starters.price,
+        category: starters.category
+    });
+    function cart_count() {
+        let cart_length = cart.length;
+        cart_target.innerHTML = `
+        ${cart_length}
+        `;
+    }
+
+    cart_count();
+}
+
+function add_to_cart(meals) {
+    cart.push({
+        id: meals.id,
+        product: meals.product,
+        Image: meals.Image,
+        price: meals.price,
+        category: meals.category
+    });
+
+    function cart_count() {
+        let cart_length = cart.length;
+        cart_target.innerHTML = `
+        ${cart_length}
+        `;
+    }
+
+    cart_count();
+}
+
+function add_to_cart(drinks) {
+    cart.push({
+        id: drinks.id,
+        product: drinks.product,
+        Image: drinks.Image,
+        price: drinks.price,
+        category: drinks.category
+    });
+
+    function cart_count() {
+        let cart_length = cart.length;
+        cart_target.innerHTML = `
+        ${cart_length}
+        `;
+    }
+
+    cart_count();
+}
+
+function add_to_cart(deserts) {
+    cart.push({
+        id: deserts.id,
+        product: deserts.product,
+        Image: deserts.Image,
+        price: deserts.price,
+        category: deserts.category
+    });
+    function cart_count() {
+        let cart_length = cart.length;
+        cart_target.innerHTML = `
+        ${cart_length}
+        `;
+    }
+
+    cart_count();
+}
+
+
+
+
     Image:"https://i.postimg.cc/RV83s81c/hot-dog.jpg",
     price:"21",
     category:"meal"
